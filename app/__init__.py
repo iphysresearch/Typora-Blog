f697cd0df1d413a6d2c035296b8e2bcb5e85e75e
Zhao Yufei <zhaoyufei@newdun.com>
2017-07-19 23:33:05 +0800

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,6 @@ from .handlers import (PageNotFoundHandler,
 
 def create_app():
     '''Create APP'''
-    print(sys.getdefaultencoding())
-    print(sys.stdout.encoding)
     root_logger = logging.getLogger()
     formatter = logging.Formatter('[%(asctime)s] $%(levelname)s (%(filename)s:%(lineno)d) %(message)s')
     stdout_hdlr = logging.StreamHandler(sys.stdout)
