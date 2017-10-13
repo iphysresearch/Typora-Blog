6ed90a67d1aaca2d0034f62c6d0d25996386a92a
Zhao Yufei <zhaoyufei@newdun.com>
2017-10-13 16:10:17 +0800

    Fix

--- app/processor.py
+++ app/processor.py
@@ -9,13 +9,20 @@ def str2timestamp(time_str):
     try:
         if ':' in time_str:
             if '/' in time_str:
-                return time_str.split('/')[0], time.mktime(datetime.datetime.strptime(time_str, '%Y/%m/%d %H:%M:%S').timetuple())
-            else:
-                return time_str.split('-')[0], time.mktime(datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S').timetuple())
+                return (time_str.split('/')[0],
+                        time.mktime(datetime.datetime.strptime(time_str,
+                                                               '%Y/%m/%d %H:%M:%S').timetuple()))
+            return (time_str.split('-')[0],
+                    time.mktime(datetime.datetime.strptime(time_str,
+                                                           '%Y-%m-%d %H:%M:%S').timetuple()))
         elif '/' in time_str:
-            return time_str.split('/')[0], time.mktime(datetime.datetime.strptime(time_str, '%Y/%m/%d %H-%M-%S').timetuple())
+            return (time_str.split('/')[0],
+                    time.mktime(datetime.datetime.strptime(time_str,
+                                                           '%Y/%m/%d %H-%M-%S').timetuple()))
         else:
-            return time_str.split('-')[0], time.mktime(datetime.datetime.strptime(time_str, '%Y-%m-%d %H-%M-%S').timetuple())
+            return (time_str.split('-')[0],
+                    time.mktime(datetime.datetime.strptime(time_str,
+                                                           '%Y-%m-%d %H-%M-%S').timetuple()))
     except Exception as err:
         options.config['root_logger'].error(err, exc_info=True)
         return 0
@@ -23,15 +30,19 @@ def str2timestamp(time_str):
 def process():
     posts = []
     new_posts = os.listdir(os.path.join(options.config['root_path'], 'post'))
-    for template_file_name in os.listdir(os.path.join(options.config['root_path'], 'app/templates/post')):
+    for template_file_name in os.listdir(os.path.join(options.config['root_path'],
+                                                      'app/templates/post')):
         if template_file_name not in new_posts:
-            os.remove(os.path.join(options.config['root_path'], 'app/templates/post', template_file_name))
+            os.remove(os.path.join(options.config['root_path'],
+                                   'app/templates/post',
+                                   template_file_name))
     for post_file_name in new_posts:
         if '.html' in post_file_name:
             post = {}
             post['title'] = post_file_name.replace('.html', '')
             post['id'] = post['title'].replace(' ', '')
-            with open(os.path.join(options.config['root_path'], 'post/%s' % post_file_name), 'r') as source_file:
+            with open(os.path.join(options.config['root_path'],
+                                   'post/%s' % post_file_name), 'r') as source_file:
                 text = source_file.read()
             soup = BeautifulSoup(text, 'lxml')
             post['year'], post['timestamp'] = str2timestamp(soup.find('p').get_text())
@@ -52,10 +63,13 @@ def process():
                 '{% end %}{% block section %}<div class="postBlock">' + \
                 str(soup.find('body')).replace('</h2>', '</h2><div class="time">\
                                                <input type="hidden" value="{{ \
-                                               timestamp }}"/></div>') + '<div id="gitalk-container"></div></div>{% end %}'
+                                               timestamp }}"/></div>') + \
+                                               '<div id="gitalk-container"></div></div>{% end %}'
             except Exception as err:
                 options.config['root_logger'].error(err, exc_info=True)
-            with open(os.path.join(options.config['root_path'], 'app/templates/post/%s.html' % post['title']), 'w') as template_file:
+            with open(os.path.join(options.config['root_path'],
+                                   'app/templates/post/%s.html' % post['title']),
+                      'w') as template_file:
                 template_file.write(template)
             posts.append(post)
     posts.sort(key=lambda x: x['timestamp'], reverse=True)
